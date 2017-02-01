84f625728c59ef0d7b9865a75795bab2290af4fe
Ryan Hennings <r.hennings11@gmail.com>
2017-01-31 22:39:47 -0800

    Protocols lab complete

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -17,10 +17,32 @@ class BankVault {
         self.name = name
         self.address = address
     }
-    
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
 
-
-
-
+extension BankVault {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        guard password.count > 1 else {
+            //print("RYAN-Pass too small")
+            return false
+        }
+        guard password.count <= 10 else {
+            //print("RYAN-Pass too large")
+            return false
+        }
+        
+        for (index, num) in password.enumerated() {
+            if index%2 == 0 { // Every other digit
+                if num%2 != 0 { // Number is odd (fails condition)
+                    //print("RYAN-Index: \(index) is not even (\(num))")
+                    return false
+                }
+            }
+        }
+        // Password meets conditions
+        return true
+    }
+}
